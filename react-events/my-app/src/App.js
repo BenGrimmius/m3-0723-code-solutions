4b1652ff8366acb5d9f17c2ec60470a927b45a63
Ben Grimmius <bgrimmdev@gmail.com>
2023-07-19 03:30:08 +0000

    Complete react-events exercise

---
+++ react-events/my-app/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import CustomButton from './CustomButton';
+
+function App() {
+  const handleCustomClick = (text) => {
+    alert(text);
+  };
+
+  return (
+    <div className="App">
+      <CustomButton
+        color="red"
+        text={'First Button!'}
+        onCustomClick={() => handleCustomClick('Red button was clicked!')}
+      />
+      <CustomButton
+        color="green"
+        text={'Second Button!'}
+        onCustomClick={() => handleCustomClick('Green button was clicked!')}
+      />
+      <CustomButton
+        color="skyBlue"
+        text={'Third Button!'}
+        onCustomClick={() => handleCustomClick('Blue button was clicked!')}
+      />
+    </div>
+  );
+}
+
+export default App;
