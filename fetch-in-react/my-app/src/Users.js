6c72ae6dfbcea4f8bd5f352dc7132d89f6927fa5
Ben Grimmius <bgrimmdev@gmail.com>
2023-08-08 02:56:05 +0000

    test

---
+++ fetch-in-react/my-app/src/Users.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-unused-vars -- Remove me */
+import React, { useEffect, useState } from 'react';
+import UserDetails from './UserDetails';
+import UserCard from './UserCard';
+import './Users.css';
+
+export default function Users() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState();
+
+  /* your code here (hint: useEffect) */
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        setIsLoading(false);
+        return res.json();
+      })
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((e) => {
+        console.error(e);
+        setError(true);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    console.error('Fetch error:', error);
+    return <p>Error! {error.message}</p>;
+  }
+  return (
+    <div className="container">
+      <UserList users={users} onClick={(user) => setUser(user)} />
+      {user && (
+        <UserDetails userId={user.id} onCancel={() => setUser(undefined)} />
+      )}
+    </div>
+  );
+}
+
+function UserList({ users, onClick }) {
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          <UserCard user={user} onClick={onClick} />
+        </li>
+      ))}
+    </ul>
+  );
+}
